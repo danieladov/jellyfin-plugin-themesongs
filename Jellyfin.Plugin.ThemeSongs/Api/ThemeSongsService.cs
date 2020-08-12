9af9fb5dc76a6ad3f1c88b7f6a1f525b5307f3bb
crobibero <cody@robibe.ro>
2020-08-12 09:46:34 -0600

    clean up

--- Jellyfin.Plugin.ThemeSongs/Api/ThemeSongsService.cs
+++ Jellyfin.Plugin.ThemeSongs/Api/ThemeSongsService.cs
@@ -1,8 +1,4 @@
 using System;
-using MediaBrowser.Api;
-using MediaBrowser.Controller.Collections;
-using MediaBrowser.Controller.Configuration;
-using MediaBrowser.Controller.Dto;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Net;
 using MediaBrowser.Model.Services;
@@ -11,48 +7,34 @@ using Microsoft.Extensions.Logging;
 namespace Jellyfin.Plugin.ThemeSongs.Api
 {
     [Route("/ThemeSongs/Download", "POST", Summary = "Downloads theme songs")]
-    
-
-    
-
     [Authenticated]
     public class DownloadRequest : IReturnVoid
     {
     }
 
-    
     public class GetId 
     {
         [ApiMember(Name = "Id", Description = "Item Id", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
         public Guid Id { get; set; }
     }
 
-    
-
     public class ThemeSongsService : IService
     {
-        private readonly ThemeSongsManager _mergeVersionsManager;
-        private readonly ILogger<VideosService> _logger;
-        private readonly IProgress<double> progress;
+        private readonly ThemeSongsManager _themeSongsManager;
+        private readonly ILogger<ThemeSongsService> _logger;
 
-        public ThemeSongsService(ILibraryManager libraryManager, ICollectionManager collectionManager, ILogger<VideosService> logger, IServerConfigurationManager serverConfigurationManager,
-            IHttpResultFactory httpResultFactory,
-            IUserManager userManager,
-            IDtoService dtoService,
-            IAuthorizationContext authContext)
+        public ThemeSongsService(
+            ILibraryManager libraryManager,
+            ILogger<ThemeSongsService> logger)
         {
-            _mergeVersionsManager = new ThemeSongsManager( libraryManager,  collectionManager,  logger,  serverConfigurationManager,
-             httpResultFactory,
-             userManager,
-             dtoService,
-             authContext,new GetId());
+            _themeSongsManager = new ThemeSongsManager(libraryManager,  logger);
             _logger = logger;
         }
         
         public void Post(DownloadRequest request)
         {
             _logger.LogInformation("Starting a manual refresh, looking up for repeated versions");
-            _mergeVersionsManager.DownloadAllThemeSongs();
+            _themeSongsManager.DownloadAllThemeSongs();
             _logger.LogInformation("Completed refresh");
         }
 
--- Jellyfin.Plugin.ThemeSongs/ThemeSongsManager.cs
+++ Jellyfin.Plugin.ThemeSongs/ThemeSongsManager.cs
@@ -1,22 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using Jellyfin.Plugin.ThemeSongs.Api;
-using MediaBrowser.Controller.Collections;
-using MediaBrowser.Controller.Configuration;
-using MediaBrowser.Controller.Dto;
 using MediaBrowser.Controller.Entities;
-using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Controller.Net;
 using MediaBrowser.Controller.Plugins;
-using MediaBrowser.Controller.Session;
 using MediaBrowser.Model.Entities;
-using MediaBrowser.Model.Querying;
 using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.ThemeSongs
@@ -25,47 +18,24 @@ namespace Jellyfin.Plugin.ThemeSongs
     {
         private readonly ILibraryManager _libraryManager;
         private readonly Timer _timer;
-        private readonly ILogger _logger; // TODO logging
-        private readonly IUserManager _userManager;
-        private readonly SessionInfo _session;
-        private readonly ISessionManager _sessionManager;
-		private ICollectionManager collectionManager;
-		private IServerConfigurationManager serverConfigurationManager;
-		private IHttpResultFactory httpResultFactory;
-		private IDtoService dtoService;
-		private IAuthorizationContext authContext;
-		private GetId getId;
+        private readonly ILogger _logger;
 
-		public ThemeSongsManager(ILibraryManager libraryManager, ICollectionManager collectionManager, ILogger logger, IServerConfigurationManager serverConfigurationManager,
-            IHttpResultFactory httpResultFactory,
-            IUserManager userManager,
-            IDtoService dtoService,
-            IAuthorizationContext authContext, GetId request)
+        public ThemeSongsManager(ILibraryManager libraryManager, ILogger logger)
         {
-            var id = request.Id;
-            _session = new SessionInfo(_sessionManager, logger);
             _libraryManager = libraryManager;
-            _userManager = userManager;
             _logger = logger;
             _timer = new Timer(_ => OnTimerElapsed(), null, Timeout.Infinite, Timeout.Infinite);
-            this.getId = getId;
         }
 
-		
-
-		private IEnumerable<Series> GetSeriesFromLibrary()
+        private IEnumerable<Series> GetSeriesFromLibrary()
         {
-            var episodes = _libraryManager.GetItemList(new InternalItemsQuery
+            return _libraryManager.GetItemList(new InternalItemsQuery
             {
-                IncludeItemTypes = new[] { nameof(Series) },
+                IncludeItemTypes = new[] {nameof(Series)},
                 IsVirtualItem = false,
                 Recursive = true,
-                HasTvdbId = true,
-
+                HasTvdbId = true
             }).Select(m => m as Series);
-
-            return episodes.ToList();
-
         }
 
         public void DownloadAllThemeSongs()
@@ -74,20 +44,19 @@ namespace Jellyfin.Plugin.ThemeSongs
             foreach (var serie in series)
             {
                 var tvdb = serie.GetProviderId(MetadataProvider.Tvdb);
-                var themeSongPath = serie.Path + "/theme.mp3";
-                var link = "http://tvthemes.plexapp.com/" + tvdb + ".mp3";
-                _logger.LogDebug($"Triying to download {serie.Name} , {link}");
+                var themeSongPath = Path.Join(serie.Path, "theme.mp3");
+                var link = $"http://tvthemes.plexapp.com/{tvdb}.mp3";
+                _logger.LogDebug("Trying to download {seriesName}, {link}", serie.Name, link);
 
                 try
                 {
-                    using (var client = new WebClient())
-                        client.DownloadFile(link, themeSongPath);
-                }catch(Exception e)
-				{
-                    _logger.LogError($"{serie.Name} not found, or not internet conection");
-                    _logger.LogError(e.Message);
-				}
-                
+                    using var client = new WebClient();
+                    client.DownloadFile(link, themeSongPath);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "{seriesName} not found, or no internet connection", serie.Name);
+                }
             }
         }
 
@@ -95,7 +64,6 @@ namespace Jellyfin.Plugin.ThemeSongs
         {
             // Stop the timer until next update
             _timer.Change(Timeout.Infinite, Timeout.Infinite);
-
         }
 
         public Task RunAsync()
@@ -105,7 +73,6 @@ namespace Jellyfin.Plugin.ThemeSongs
 
         public void Dispose()
         {
-
         }
     }
 }
